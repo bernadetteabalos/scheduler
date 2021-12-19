aa7d837bc93d7a63ac56056f40fcf10d5c2d9d31
Bernadette <bern.roseabalos@gmail.com>
2021-12-19 13:44:35 -0500

    Refactor formatSpots function to avoid variable name collision

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,19 +2,20 @@ import React from "react";
 import 'components/DayListItem.scss';
 import classNames from "classnames";
 
-export default function DayListItem(props) {
-  function formatSpots(props) {
-    let spotsRemaining = ""
-    if (props.spots === 0) {
-      spotsRemaining += 'no spots remaining'
-    } else if (props.spots === 1) {
-      spotsRemaining += '1 spot remaining'
-    } else {
-      spotsRemaining += props.spots + ' spots remaining'
-    }
-    return spotsRemaining;
-  };
+function formatSpots(spots) {
+  let spotsRemaining = ""
+  if (spots === 0) {
+    spotsRemaining += 'no spots remaining'
+  } else if (spots === 1) {
+    spotsRemaining += '1 spot remaining'
+  } else {
+    spotsRemaining += spots + ' spots remaining'
+  }
+  return spotsRemaining;
+};
 
+export default function DayListItem(props) {
+  
   let dayClass = classNames({
     'day-list__item': true,
     'day-list__item--selected': props.selected,
@@ -27,7 +28,7 @@ export default function DayListItem(props) {
       onClick={() => props.setDay(props.name)}
       data-testid="day">
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{formatSpots(props)}</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 };

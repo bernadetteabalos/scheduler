6c65b0c8e20314aff870fca716052c046a400069
Bernadette <bern.roseabalos@gmail.com>
2021-12-19 13:46:16 -0500

    Fix hard coding of URL when making calls to API

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -52,7 +52,7 @@ export default function useApplicationData() {
     days[indexTargetDayObj].spots = spots;
 
     //Axios put request to save interview into database so it doesn't reset when we refresh
-    return axios.put(`http://localhost:8001/api/appointments/${id}`, { interview })
+    return axios.put(`/api/appointments/${id}`, { interview })
       .then(() => {
         setState({ ...state, appointments, days });
       });
@@ -86,7 +86,7 @@ export default function useApplicationData() {
     days[indexTargetDayObj].spots = spots;
 
     //Delete request to remove appointment from database
-    return axios.delete(`http://localhost:8001/api/appointments/${id}`)
+    return axios.delete(`/api/appointments/${id}`)
       .then(() => {
         setState({ ...state, appointments, days });
       });
@@ -95,9 +95,9 @@ export default function useApplicationData() {
   //Axios get requests to grab database info from api server
   useEffect(() => {
     Promise.all([
-      axios.get('http://localhost:8001/api/days'),
-      axios.get('http://localhost:8001/api/appointments'),
-      axios.get('http://localhost:8001/api/interviewers')
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
     ]).then((all) => {
       setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     });
